1e53741e3801e33b79e09b6b6a1cfd15e899287d
vaad23 <vaad23@mail.ru>
2020-04-21 13:08:42 +0300

    Version 01

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private GameObject _enemy;
+
+    private void Awake()
+    {
+        StartCoroutine(CreateEnemyInRandomPoint());
+    }
+
+    private IEnumerator CreateEnemyInRandomPoint()
+    {
+        var timer = new WaitForSeconds(2f);
+
+        while (true)
+        {
+            if (_spawnPoints.Length > 0 && _enemy!= null)
+            {
+                Transform position = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+                Instantiate(_enemy, position);
+            }
+            yield return timer;
+        }
+    }
+}
